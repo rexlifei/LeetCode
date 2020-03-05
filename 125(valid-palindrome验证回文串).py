bab82512fafb571790834f08418aa063d8606b8d
rexlifei <rexlifei@gmail.com>
2020-03-05 15:56:40 +0800

    Update 125(valid-palindrome验证回文串).py

--- 125(valid-palindrome验证回文串).py
+++ 125(valid-palindrome验证回文串).py
@@ -1,6 +1,8 @@
 '''
 https://leetcode-cn.com/problems/valid-palindrome/
 思路：先把数字和字母提取出来，然后反转，判断是否和原字符串一致。
+
+改进：可以尝试只对比前半段和后半段的反转，判断是否一致。
 '''
 class Solution(object):
     def isPalindrome(self, s):
