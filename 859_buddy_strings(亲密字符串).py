d85381038191ad51e376202e2b5c7585f885131a
rexlifei <rexlifei@gmail.com>
2020-03-06 11:53:17 +0800

    Update 859_buddy_strings(亲密字符串).py

--- 859_buddy_strings(亲密字符串).py
+++ 859_buddy_strings(亲密字符串).py
@@ -6,8 +6,6 @@ https://leetcode-cn.com/problems/buddy-strings/
 1. 如果长度不等，返回false
 2. 如果字符串相同，并且一个字符串里面有重复字符，返回true
 3. 找到两个字符串不一样的地方，肯定是有两个点位不一样，并且字符相反
-
-
 '''
 
 class Solution(object):
@@ -20,14 +18,19 @@ class Solution(object):
         if len(A) != len(B):
             return False
 
-        if A == B and len(set(A)) < len(A):
-            return True
+        if A == B:
+            if len(set(A)) < len(A):
+                return True
+            else:
+                return False
         
         pair = list()
         for i in range(len(A)):
             if A[i] != B[i]:
                 pair.append([A[i], B[i]])
-        
+                if len(pair) > 2:
+                    return False
+                
         if len(pair) != 2:
             return False
         
